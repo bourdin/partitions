b27592ec56d1572c4789dc474be65e93918386b6
Blaise Bourdin <bourdin@lsu.edu>
2008-06-16 15:59:58 +0000

    Added pfigure.m

---
+++ Code/pfigure.m
@@ -0,0 +1,10 @@
+function pfigure(num)
+
+switch(num)
+    case (1)
+        figure(1)
+        set(1,'Position', [0, 843, 690, 666]);
+    case(2)
+        figure(2)
+        set(2, 'Position', [692, 843, 638, 666]);
+end
